253617f6ff40e3abaf81e534f7f4013f15d98a4a
Han-Skywalker <1312692068@qq.com>
2024-03-07 14:36:09 +0800

    add examine

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import setting
 
 args = setting.parse_args()
+setting.examine_isa(args)
--- setting.py
+++ setting.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 
 
 def parse_args():
@@ -7,3 +8,16 @@ def parse_args():
     parser.add_argument('--bits', type=int, default=32, help='Set the bits of processor')
     parser.add_argument('--mode', type=str, default='inside', help='Choose the way to test')
     return parser.parse_args()
+
+
+def examine_isa(args):
+    if args.bits != 32 and args.bits != 64:
+        print('The bits of processor are not available now!')
+        sys.exit(2)
+    if args.mode == 'outside':
+        print('The outside mode is not available now!')
+        sys.exit(3)
+    if args.ISA != 'risc-v':
+        print('The ISA is not available now!')
+        sys.exit(1)
+        
